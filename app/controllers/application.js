f7e6b0fe9ec30138cd43e55f0cc969158e7fbcdd
Dilip <dilip@artoo.in>
2016-02-25 11:27:58 +0530

    addCard functionality.

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -3,7 +3,13 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   actions: {
     addCard(){
-      
+      let content = this.get('content');
+      content.pushObject(
+        Ember.Object.create({
+        title: '',
+        description: '',
+        checklist: []
+      }));
     }
   }
 });
--- app/routes/application.js
+++ app/routes/application.js
@@ -3,20 +3,20 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model: function() {
     return [
-      {
+      Ember.Object.create({
         title: 'First card', 
         description: 'This is the most important thing to do today',
         checklist: [
-          {
+          Ember.Object.create({
             done: false,
             text: 'first item'
-          },
-          {
+          }),
+          Ember.Object.create({
             done: false,
             text: 'second item'
-          }
+          })
         ]
-      }
+      })
     ];
   },
 });
